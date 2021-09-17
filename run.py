3a14a88b85c27fd5289971a470437cc93912f64d
andreaslindgren <andreas.lindgren@nacka.se>
2021-09-17 19:44:24 +0200

    nyttinygren

--- run.py
+++ run.py
@@ -1,8 +1,1 @@
-print('Hello worldis!!!')
-print('Hello world!')
-
-Jaås?
-Who would have thought?
-
-Det hade jag väl aänåd aldrig kunnat tra?
-Det hade jag väl aänåd aldrig kunnat tru??
+Helt nytt här!
